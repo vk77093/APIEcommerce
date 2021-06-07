0db2de6841569c9e83ee0111cc296205b1d990f7
vijay kumar <vk77093@gmail.com>
2021-06-07 12:42:18 +0530

    realtionship added

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -8,4 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Review extends Model
 {
     use HasFactory;
+    public function products(){
+        return $this->belongsTo(Product::class);
+    }
 }
